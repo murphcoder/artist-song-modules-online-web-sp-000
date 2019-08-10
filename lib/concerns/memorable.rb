77da37eb91ac2916abb6bcd9f3d6e434483bcaf5
Sean Murphy <filmmakersean@fastmail.net>
2019-08-10 01:38:34 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,23 @@
 module Memorable
   
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+  
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
-  end  
+    def count
+      self.all.count
+    end
+    
+  end
+  
+  module InstanceMethods
+    
+    def initialize
+      
+    end
+    
+  end
   
 end
